9cc2b59ea58c856bfcc2c044dbc771cc9aa6b1e4
Yi Wang <yi.wang.2005@gmail.com>
2020-02-05 14:27:38 -0800

    Remove experiment.bash

--- experiment.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-(cd segment && go build && ./segment /tmp/crawl > /tmp/data)
-(cd plda && ./lda --num_topics 50 --alpha 0.1 --beta 0.01 --training_data_file /tmp/data --model_file /tmp/model --burn_in_iterations 100 --total_iterations 150 && ./view_model.py /tmp/model > /tmp/readable)
