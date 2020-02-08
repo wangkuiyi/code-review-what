0fffb32473ffc85a34b0e0a4b4f46c7c40168ad1
Yi Wang <yi.wang.2005@gmail.com>
2020-02-08 09:51:33 -0800

    Add comment to crawler.go

--- crawl/crawl.go
+++ crawl/crawl.go
@@ -1,3 +1,43 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed
+// with this work for additional information regarding copyright
+// ownership.  The ASF licenses this file to you under the Apache
+// License, Version 2.0 (the "License"); you may not use this file
+// except in compliance with the License.  You may obtain a copy of
+// the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.  See the License for the specific language governing
+// permissions and limitations under the License.
+
+// To build this crawler, you need to install Go and run the following
+// command.
+//
+//   go install ./...
+//
+// To crawl the pull requests and comments of all pull requests,
+// please run the following command.
+//
+//  $GOPATH/bin/crawler -user your_github_account -passwd your_github_passwd
+//
+// Or, you can set your username and password in environment variables.
+//
+//  export GITHUB_USER=your_github_account
+//  export GITHUB_PASSWD=your_github_passwd
+//  $GOPATH/bin/crawler
+//
+// This crawler generates two CSV files: pulls.csv and comments.csv.
+// Both have a column named number, which is the pull request number.
+//
+// You can import these two files into a SQL database, for example,
+// MySQL, as two tables, you can join these two tables by number.  For
+// more about the importing and NLP with MySQL, please refer to
+// https://medium.com/@yi.wang.2005/nlp-in-sql-word-vectors-82dffc908423
+//
 package main
 
 import (
